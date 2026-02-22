52850ec836f8e50fe70cd1d3265dc49dfc563406
Rafael <rafael.canal.ufsc@gmail.com>
2026-02-22 17:25:54 -0300

    refactor: consolidate research tools into attacks/ and benchmarks/ subfolders

--- external/misc-benchmarks/README.md
+++
@@ -1,6 +0,0 @@
-# Misc micro-benchmarks & tools
-
-## Detecting DRAM address mapping
-
-See README-map-detector.md
-
--- external/misc-benchmarks/init-hugetlbfs.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-mount | grep hugetlbfs || mount -t hugetlbfs none /mnt/huge
-echo 2048 > /proc/sys/vm/nr_hugepages
-
